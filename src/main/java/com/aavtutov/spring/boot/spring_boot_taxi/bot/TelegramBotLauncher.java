645468d49d1043a6fc1c9666dee6e3fd0d355c00
Andrei Avtutov <aavtutov@gmail.com>
2025-09-29 19:58:14 +0300

    feat(bot): add bot values in properties and message methods

--- src/main/java/com/aavtutov/spring/boot/spring_boot_taxi/bot/TelegramBotLauncher.java
+++ src/main/java/com/aavtutov/spring/boot/spring_boot_taxi/bot/TelegramBotLauncher.java
@@ -1,23 +1,51 @@
 package com.aavtutov.spring.boot.spring_boot_taxi.bot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class TelegramBotLauncher extends TelegramLongPollingBot {
 
+	@Value("${telegram.bot.username}")
+	private String botUserName;
+
+	public TelegramBotLauncher(@Value("${telegram.bot.token}") String botToken) {
+		super(botToken);
+	}
+
 	@Override
 	public void onUpdateReceived(Update update) {
-		// TODO Auto-generated method stub
-		
+
+		if (update.hasMessage() && update.getMessage().hasText()) {
+			String chatId = update.getMessage().getChatId().toString();
+			String messageText = update.getMessage().getText();
+
+			if (messageText.equals("/start")) {
+				sendMessage(chatId, "Добро пожаловать в Taxi App! Нажмите кнопку для заказа.");
+			}
+
+		}
+
 	}
 
 	@Override
 	public String getBotUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return botUserName;
 	}
-	
+
+	private void sendMessage(String chatId, String messageText) {
+		SendMessage message = new SendMessage(chatId, messageText);
+
+		try {
+			execute(message);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
