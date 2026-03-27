cfaf41e1002f7b395125cc9d62a63ec2ee9f9234
Andrei Avtutov <aavtutov@gmail.com>
2026-03-27 21:39:29 +0200

    feat: add WebSocket configuration using STOMP and SockJS

--- src/main/java/com/aavtutov/spring/boot/spring_boot_taxi/config/WebSocketConfig.java
+++ src/main/java/com/aavtutov/spring/boot/spring_boot_taxi/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.aavtutov.spring.boot.spring_boot_taxi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,18 +11,19 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	
+	@Value("${web.app.url}")
+	private String webAppUrl;
+	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/topic");
 		config.setApplicationDestinationPrefixes("/app");
 	}
 	
-	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws-taxi")
-		.setAllowedOriginPatterns("*")
+		registry.addEndpoint("/api/ws-taxi")
+		.setAllowedOriginPatterns(webAppUrl)
 		.withSockJS();
 	}
-
 }
