4a901395cb8c020162f9fe4ae52a68cafb35d9fb
Andrei Avtutov <aavtutov@gmail.com>
2026-03-27 16:19:05 +0200

    feat: configure WebSocket STOMP endpoint and message broker

---
+++ src/main/java/com/aavtutov/spring/boot/spring_boot_taxi/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.aavtutov.spring.boot.spring_boot_taxi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
+	
+	
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws-taxi")
+		.setAllowedOriginPatterns("*")
+		.withSockJS();
+	}
+
+}
