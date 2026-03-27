3a358a800a43c998edad39d570bef47a99ec3652
Andrei Avtutov <aavtutov@gmail.com>
2026-03-27 21:28:44 +0200

    refactor: remove unused imports

--- src/main/java/com/aavtutov/spring/boot/spring_boot_taxi/SpringBootTaxiApplication.java
+++ src/main/java/com/aavtutov/spring/boot/spring_boot_taxi/SpringBootTaxiApplication.java
@@ -2,7 +2,6 @@ package com.aavtutov.spring.boot.spring_boot_taxi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
